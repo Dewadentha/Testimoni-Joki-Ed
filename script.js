e62882117b0607fbdf4acdf0ac4f2718afcf9842
Dentha Dewangga Prasetyo <dentha_prasetyo_33tkj@student.smktelkom-mlg.sch.id>
2026-03-26 20:54:15 +0700

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-fetch("https://api.sheetbest.com/sheets/1ec1ec4c-ed02-4c9c-b2f5-1aa26bae9b95")
+const url = "https://api.sheetbest.com/sheets/1ec1ec4c-ed02-4c9c-b2f5-1aa26bae9b95";
+
+fetch(url)
+
 .then(res => res.json())
+
 .then(data => {
 
-let container = document.getElementById("testimoni")
+let container = document.getElementById("testimoni-container");
+
+container.innerHTML = "";
 
 data.forEach(item => {
 
 container.innerHTML += `
+
 <div class="card">
+
 <p class="rating">${item.Rating}</p>
+
 <p>${item.Testimoni}</p>
+
 <span>${item.Nama}</span>
+
 </div>
+
 `
 
 })
